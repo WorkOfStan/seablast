66f7c24e23a6d59d153d153da2204193fc4a6152
WorkOfStan <rejthar@stanislavrejthar.com>
2023-12-30 01:04:56 +0100

    Update defineAppDir.php

--- defineAppDir.php
+++ defineAppDir.php
@@ -1,10 +1,11 @@
 <?php
 
 // Keep this in a separate file in order to avoid declaring new symbols in index.php
-if (file_exists(__DIR__ . '/../../autoload.php')) {
-    // called as library
-    define('APP_DIR', __DIR__ . '/../../..');
-} else {
-    // direct usage (TODO: consider whether necessary at all)
-    define('APP_DIR', __DIR__);
-}
+define(
+    'APP_DIR',
+    file_exists(__DIR__ . '/../../autoload.php')
+        // called as library
+        ? __DIR__ . '/../../..'
+        // direct usage (TODO: consider whether necessary at all)
+        : __DIR__
+);
