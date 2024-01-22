ff8a3255fc00bfcdc28702a3a3564bbafd2d7bb5
WorkOfStan <rejthar@stanislavrejthar.com>
2024-01-22 22:09:48 +0100

    CSRF

--- src/SeablastModel.php
+++ src/SeablastModel.php
@@ -9,6 +9,8 @@ use Webmozart\Assert\Assert;
 use Seablast\Seablast\SeablastController;
 use Seablast\Seablast\Superglobals;
 use stdClass;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
 
 class SeablastModel
 {
@@ -16,10 +18,10 @@ class SeablastModel
 
     /** @var SeablastController */
     private $controller;
-    /** @var string[] mapping of URL to processing */
-    public $mapping;
-    /** @var array<mixed>|stdClass TODO: use only object instead */
-    private $viewParameters = []; // null;
+    /** @ var string[] mapping of URL to processing */
+    //public $mapping;
+    /** @var stdClass */
+    private $viewParameters!
 
     /**
      *
@@ -30,13 +32,21 @@ class SeablastModel
     {
         $this->controller = $controller;
         Debugger::barDump($this->controller, 'Controller'); // debug
-        $this->mapping = $this->controller->mapping;
-        if (isset($this->mapping['model'])) {
-            $className = $this->mapping['model'];
-            $m = new $className($this->controller->getConfiguration(), $superglobals);
-            Assert::methodExists($m, 'knowledge', "{$className} model MUST have method knowledge()");
-            $this->viewParameters = $m->knowledge();
+        //$this->mapping = $this->controller->mapping;
+        // todo $this->mapping is redundant
+        if (isset($this->controller->mapping['model'])) {
+            $className = $this->controller->mapping['model'];
+            $model = new $className($this->controller->getConfiguration(), $superglobals);
+            Assert::methodExists($model, 'knowledge', "{$className} model MUST have method knowledge()");
+            $this->viewParameters = $model->knowledge();
+        // todo pokud potřeba
+        //} else {
+        //    $this->viewParemeters = new stdClass();
         }
+        // CSRF token to be used by view
+        $csrfTokenManager = new CsrfTokenManager();
+        $this->viewParameters->csrfToken = $csrfTokenManager->getToken('sb_json');
+        // todo kdy invalidovat? určitě při logout
     }
 
     /**
@@ -49,8 +59,8 @@ class SeablastModel
     }
 
     /**
-     * TODO: change to object as parameters for Latte render (yes, Latte supports object even for PHP7.2 in 2.x latest)
-     * @return array<mixed>|stdClass
+     * Parameters for Latte render (yes, Latte supports object even for PHP7.2 in 2.x latest)
+     * @return stdClass
      */
     public function getParameters()
     {
