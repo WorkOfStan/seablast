c54e4c4eb2fac4889b799cfa810076ee4ee8183e
WorkOfStan <rejthar@stanislavrejthar.com>
2024-07-16 21:43:19 +0200

    Logging level = 3

--- conf/default.conf.php
+++ conf/default.conf.php
@@ -11,18 +11,11 @@ use Seablast\Seablast\SeablastConstant;
 return static function (SeablastConfiguration $SBConfig): void {
     $SBConfig->flag
         ->activate(SeablastConstant::FLAG_WEB_RUNNING) // debug
-        //->activate('as') // debug
-        //->deactivate('mon') // debug
         ->deactivate(SeablastConstant::ADMIN_MAIL_ENABLED) // default is not sending emails to admin
         ->deactivate(SeablastConstant::USER_MAIL_ENABLED) // default is not sending emails to users
         //->activate(SeablastConstant::FLAG_DEBUG_JSON) // JSON would be displayed directly with Tracy
     ;
     $SBConfig
-        // Debug
-        //->setInt('a', 23)
-        //->setInt('b', 45)
-        //->setString('test-string', 'default-value') // debug
-        //->setArrayString('test-array-string', ['a', 'y', 'omega'])
         // Environment
         ->setInt(SeablastConstant::SB_ERROR_REPORTING, E_ALL & ~E_NOTICE)
         ->setInt(SeablastConstant::SB_SESSION_SET_COOKIE_LIFETIME, 60 * 60 * 24 * 2) // 2 days
@@ -35,6 +28,7 @@ return static function (SeablastConfiguration $SBConfig): void {
         ->setString(SeablastConstant::SB_INI_SET_SESSION_USE_STRICT_MODE, '1')
         ->setString(SeablastConstant::SB_INI_SET_DISPLAY_ERRORS, '0') // errors only in the log; override locally
         ->setArrayString(SeablastConstant::DEBUG_IP_LIST, []) // default list with IPs to show Tracy
+        ->setInt(SeablastConstant::SB_LOGGING_LEVEL, 3) // log warnings and more severe events
         // Database
         // Does not have to be 'testing' expected by the automatic GitHub tests as environment is set in the unit tests
         // like this: `$setup->getConfiguration()->setString(SeablastConstant::SB_PHINX_ENVIRONMENT, 'testing');`
@@ -42,7 +36,7 @@ return static function (SeablastConfiguration $SBConfig): void {
         // Latte templates
         ->setString(SeablastConstant::LATTE_TEMPLATE, 'views')
         ->setString(SeablastConstant::LATTE_CACHE, APP_DIR . '/cache')
-        // Error API is always available if not overriden
+        // Error API is always available (if not overriden)
         ->setArrayArrayString(
             SeablastConstant::APP_MAPPING,
             '/api/error', // todo demonstrate in SB-dist
@@ -50,7 +44,7 @@ return static function (SeablastConfiguration $SBConfig): void {
                 'model' => '\Seablast\Seablast\Apis\ApiErrorModel',
             ]
         )
-        // Error page is always available if not overriden
+        // Error page is always available (if not overriden)
         ->setArrayArrayString(
             SeablastConstant::APP_MAPPING,
             '/error', // todo demonstrate in SB-dist
