d37fc784025ecc1d15ffa4bffc0ecaf536eb4bc3
WorkOfStan <rejthar@stanislavrejthar.com>
2024-08-04 22:15:25 +0200

    Update SeablastConfigurationTest.php

--- Test/SeablastConfigurationTest.php
+++ Test/SeablastConfigurationTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Seablast\Seablast\Test;
+
 use PHPUnit\Framework\TestCase;
 use Seablast\Seablast\Exceptions\DbmsException;
 use Seablast\Seablast\SeablastConfiguration;
