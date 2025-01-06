b83d1ac0ed755c92fd8442ba8111aac74f98b80e
WorkOfStan <rejthar@stanislavrejthar.com>
2025-01-06 01:19:55 +0100

    Update SeablastMysqliStmt.php cleanup

--- src/SeablastMysqliStmt.php
+++ src/SeablastMysqliStmt.php
@@ -17,14 +17,10 @@ class SeablastMysqliStmt extends mysqli_stmt
   
     /** @var array<scalar> */
     private $boundParams = [];
-    /** @//var string */
-  //  private $logPath;
     /** @var SeablastMysqli */
     private $mysqli;
     /** @var string */
     private $query;
-    /** @//var string */
-//    private $user = 'unidentified';
 
     /**
      * Constructor that wraps an existing mysqli_stmt object.
@@ -42,9 +38,6 @@ class SeablastMysqliStmt extends mysqli_stmt
 
         // Copy internal state from the original statement
         $this->initFrom($stmt);
-
-        // Use Debugger::$logDirectory instead of APP_DIR . '/log'
-        $this->logPath = Debugger::$logDirectory . '/query_' . date('Y-m') . '.log';
     }
 
     /**
@@ -70,81 +63,38 @@ class SeablastMysqliStmt extends mysqli_stmt
         return parent::bind_param($types, ...$params);
     }
 
-    public function execute()
+    public function execute(): bool
     {
-        $compiledQuery = $this->logThisQuery();
+        $compiledQuery = $this->compileQuery();
         // Log the SQL query
-        if (!$this->isReadDataTypeQuery($trimmedQuery)) {
+        if (!$this->isReadDataTypeQuery($compiledQuery)) {
             // Log queries that may change data
             // TODO jak NELOGOVAT hesla? Použít queryNoLog() nebo nějaká chytristika?
-$this->mysqli->logQuery($compiledQuery); // Logs and returns the full query with bound values
+            $this->mysqli->logQuery($compiledQuery); // Logs and returns the full query with bound values
         }
-                try {
-                    // Execute the statement
-        $result = parent::execute();
-                    $this->mysqli->addStatement((bool) $result, $compiledQuery, $this);
-                    /*
-// todo dry logging instead of
-                    Debugger::log($compiledQuery, 'compiled query');
-if ($result === false) {
-    //echo "Error: " . parent::error;
-    // todo dry
-    $dbError = ['query' => $compiledQuery, 'Err#' => $this->errno, 'Error:' => $this->error];
-                Debugger::barDump($dbError, 'Database error', [Dumper::TRUNCATE => 1500]); // longer than 150 chars
-                Debugger::log('Database error' . print_r($dbError, true), ILogger::ERROR);
-                // todo end the rest of SeablastMysqli::query logging
-}
-                    */
-        return $result;
-                    } catch (\mysqli_sql_exception $e) {
+        try {
+            // Execute the statement
+            $result = parent::execute();
+            $this->mysqli->addStatement((bool) $result, $compiledQuery, $this);
+            return $result;
+        } catch (\mysqli_sql_exception $e) {
             // Catch any mysqli_sql_exception and throw it as DbmsException
             throw new DbmsException("mysqli_sql_exception: " . $e->getMessage(), $e->getCode(), $e);
-                }
+        }
     }
     
     /**
      * The mysqli_stmt class does not directly provide a method to return the final compiled SQL statement with bound parameters substituted. This is because parameter substitution happens at the database engine level for performance and security reasons. However, we can achieve this functionality by manually substituting parameters in the query string ourselves.
      */
-    public function logThisQuery(): string
+    private function compileQuery(): string
     {
         $loggedQuery = $this->query;
         foreach ($this->boundParams as $param) {
             $value = is_numeric($param) ? $param : "'" . self::escapeParam($param) . "'";
             $loggedQuery = preg_replace('/\?/', $value, $loggedQuery, 1);
         }
-
-        //file_put_contents('query.log', $loggedQuery . PHP_EOL, FILE_APPEND);
-
         return trim($loggedQuery);
     }
-    /**
-     * Log this query.
-     *
-     * //@ param string $query
-     * @return void
-     */
-/*    private function logQuery(): void
-    {
-        //mb_ereg_replace does not destroy multi-byte characters such as character Č
-        error_log(
-            mb_ereg_replace("\r\n|\r|\n", ' ', $this->logThisQuery()) . ' -- [' . date('Y-m-d H:i:s') . '] [' . $this->user . ']'
-            . PHP_EOL,
-            3,
-            $this->logPath
-        );
-    }
-
-    /**
-     * DI setter.
-     *
-     * @param int|string $user
-     *
-     * @return void
-     */
- /*   public function setUser($user): void
-    {
-        $this->user = (string) $user;
-    }*/
 
     private static function escapeParam($param): string
     {
